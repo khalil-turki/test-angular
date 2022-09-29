947353ad0070d92f6073db1874d7bbebc0cf0145
khalil <khalil.turki@esprit.tn>
2022-09-29 15:08:20 +0200

    FirstCommit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { ListUserComponent } from './list-user/list-user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TestComponent,
+    ListUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
